16ea3e596c427b06782bd78944d77a300a7b498b
Xiao Zeng <xiazen@microsoft.com>
2016-08-23 14:24:52 -0700

    Update HelloWorld sample
    - made HelloWorld.SeprarateProcess sln and HelloWorld.SingleProcess sln
    - in HelloWorld.SeprarteProcess sln, orleans client and host running in
      two sepaeate process and has its own project
    - in HelloWorld.SingleProcess sln, it is the old HelloWorld Sample
    - update Orleans.SeparateProcess to use orleans 1.2.3 version

---
+++ Samples/HelloWorld/OrleansClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HelloWorld.Interfaces;
+using Orleans;
+using Orleans.Runtime;
+using Orleans.Runtime.Configuration;
+
+namespace OrleansClient
+{
+    /// <summary>
+    /// Orleans test silo client
+    /// </summary>
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            var config = ClientConfiguration.LocalhostSilo();
+            const int initializeAttemptsBeforeFailing = 5;
+            int attemp = 0;
+            while (true)
+            {
+                try
+                {
+                    GrainClient.Initialize(config);
+                    Console.WriteLine("Client successfully connect to silo host");
+                    break;
+                }
+                catch (SiloUnavailableException e)
+                {
+                    attemp++;
+                    if (attemp > initializeAttemptsBeforeFailing)
+                    {
+                        Console.WriteLine("Connect to silo host failed due to " + e);
+                        Console.WriteLine("Press Enter to terminate...");
+                        Console.ReadLine();
+                        return 1;
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                }
+            }
+
+            DoClientWork().Wait();
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+            return 0;
+
+        }
+
+        private static async Task DoClientWork()
+        {
+            var friend = GrainClient.GrainFactory.GetGrain<IHello>(0);
+            var response = await friend.SayHello("Good morning, my friend!");
+            Console.WriteLine("\n\n{0}\n\n", response);
+        }
+
+    }
+}
---
+++ Samples/HelloWorld/OrleansHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OrleansHost
+{
+    /// <summary>
+    /// Orleans test host
+    /// </summary>
+    public class Program
+    {
+        private static OrleansHostWrapper hostWrapper;
+        static int Main(string[] args)
+        {
+            int returnCode = StartSilo(args);
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+
+            returnCode += ShutdownSilo();
+            return returnCode;//either StartSilo or ShutdownSilo failed would result on a non-zero return code. 
+        }
+
+
+        private static int StartSilo(string[] args)
+        {
+            hostWrapper = new OrleansHostWrapper(args);
+            return hostWrapper.Run();
+        }
+
+        private static int ShutdownSilo()
+        {
+            if (hostWrapper != null)
+            {
+                return hostWrapper.Stop();
+            }
+            return 0;
+        }
+    }
+}
