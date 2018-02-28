bdb7dd778e4d999f9a603babc6981a44ce2fd4f2
xiazen <xiazen@microsoft.com>
2018-02-28 15:57:42 -0800

    streaming builder prototype

--- src/Orleans.Runtime.Abstractions/Hosting/StreamHostingExtensions.cs
+++ src/Orleans.Runtime.Abstractions/Hosting/StreamHostingExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Orleans.Runtime;
 using Orleans.Hosting;
 using Orleans.Streams;
@@ -10,6 +11,142 @@ using Orleans.Providers.Streams.SimpleMessageStream;
 
 namespace Orleans.Hosting
 {
+    public interface IPersistentStreamProviderConfigurator : INamedServiceConfigurator
+    {
+        PersistentStreamProviderBuilder ConfigureQueueAdapter<TOptions, TQueueAdapter>(
+            Action<TOptions> configureOptions)
+            where TQueueAdapter : class, IQueueAdapter
+            where TOptions : class, new();
+
+        PersistentStreamProviderBuilder ConfigureQueueBalancer<TOptions, TQueueBalancer>(
+            Action<TOptions> configureOptions)
+            where TQueueBalancer : class, IStreamQueueBalancer
+            where TOptions : class, new();
+
+        PersistentStreamProviderBuilder ConfigureQueueAdapterReceiver<TOptions, TQueueAdapterReceiver>(
+            Action<TOptions> configureOptions)
+            where TQueueAdapterReceiver : class, IQueueAdapterReceiver
+            where TOptions : class, new();
+
+        PersistentStreamProviderBuilder ConfigureQueueMapper<TOptions, TQueueMapper>(Action<TOptions> configureOptions)
+            where TQueueMapper : class, IStreamQueueMapper
+            where TOptions : class, new ();
+    }
+
+    public interface INamedServiceConfigurator
+    {
+        INamedServiceConfigurator ConfigureComponent<TOptions, TComponent>(Action<TOptions> configureOptions)
+            where TComponent : class
+            where TOptions : class, new();
+    }
+
+    public class NamedServiceConfigurator : INamedServiceConfigurator
+    {
+        private readonly string name;
+        private readonly IServiceCollection services;
+        private readonly ISiloHostBuilder builder;
+        public NamedServiceConfigurator(string name, IServiceCollection services)
+        {
+            this.name = name;
+            this.services = services;
+        }
+
+        public NamedServiceConfigurator(string name, ISiloHostBuilder builder)
+        {
+            this.name = name;
+            this.builder = builder;
+        }
+
+        public INamedServiceConfigurator ConfigureComponent<TOptions, TComponent>(Action<TOptions> configureOptions)
+            where TComponent : class
+            where TOptions : class, new()
+        {
+            if (services != null)
+            {
+                services.Configure<TOptions>(name, configureOptions);
+                services.AddSingletonNamedService<TComponent>(name, (s, n) => ActivatorUtilities.CreateInstance<TComponent>(s, name, s.GetService<IOptionsSnapshot<TOptions>>().Get(name)));
+            }
+            if (builder != null)
+            {
+                builder.ConfigureServices(s =>
+                {
+                    s.Configure<TOptions>(name, configureOptions);
+                    s.AddSingletonNamedService<TComponent>(name,
+                        (sp, n) => ActivatorUtilities.CreateInstance<TComponent>(sp, name,
+                            sp.GetService<IOptionsSnapshot<TOptions>>().Get(name)));
+                });
+            }
+            return this;
+        }
+    }
+
+    public class PersistentStreamProviderBuilder : IPersistentStreamProviderConfigurator
+    {
+        private readonly NamedServiceConfigurator namedServiceBuilder;
+        public PersistentStreamProviderBuilder(string name, IServiceCollection services)
+        {
+            this.namedServiceBuilder = new NamedServiceConfigurator(name, services);
+        }
+
+        public PersistentStreamProviderBuilder(string name, ISiloHostBuilder builder)
+        {
+            this.namedServiceBuilder = new NamedServiceConfigurator(name, builder);
+        }
+
+        public PersistentStreamProviderBuilder ConfigureQueueAdapter<TOptions, TQueueAdapter>(Action<TOptions> configureOptions)
+            where TQueueAdapter: class, IQueueAdapter
+            where TOptions : class, new()
+        {
+            namedServiceBuilder.ConfigureComponent<TOptions, TQueueAdapter>(configureOptions);
+            return this;
+        }
+
+        public PersistentStreamProviderBuilder ConfigureQueueBalancer<TOptions, TQueueBalancer>(Action<TOptions> configureOptions)
+            where TQueueBalancer : class, IStreamQueueBalancer
+            where TOptions : class, new()
+        {
+            namedServiceBuilder.ConfigureComponent<TOptions, TQueueBalancer>(configureOptions);
+            return this;
+        }
+
+        public PersistentStreamProviderBuilder ConfigureQueueAdapterReceiver<TOptions, TQueueAdapterReceiver>(Action<TOptions> configureOptions)
+            where TQueueAdapterReceiver : class, IQueueAdapterReceiver
+            where TOptions : class, new()
+        {
+            namedServiceBuilder.ConfigureComponent<TOptions, TQueueAdapterReceiver>(configureOptions);
+            return this;
+        }
+
+        public PersistentStreamProviderBuilder ConfigureQueueMapper<TOptions, TQueueMapper>(Action<TOptions> configureOptions)
+            where TQueueMapper : class, IStreamQueueMapper
+            where TOptions : class, new()
+        {
+            namedServiceBuilder.ConfigureComponent<TOptions, TQueueMapper>(configureOptions);
+            return this;
+        }
+        
+        void Validate()
+        {
+            //validate if all components configured
+        }
+    }
+
+    public static class PersistentStreamProviderBuilderExtensions
+    {
+        public static PersistentStreamProviderBuilder AddPersistentStream(this ISiloHostBuilder builder, string name)
+        {
+            var providerBuilder =  new PersistentStreamProviderBuilder(name, builder);
+            return providerBuilder;
+        }
+
+        public static PersistentStreamProviderBuilder AddPersistentStreams(this IServiceCollection services, string name)
+        {
+            var builder = new PersistentStreamProviderBuilder(name, services);
+            //services.AddSingletonNamedService<PersistentStreamProviderBuilder>(name, (s, n)=>builder);
+            return builder;
+        }
+    }
+
     public static class StreamHostingExtensions
     {
         /// <summary>
---
+++ src/Orleans.Runtime/Streams/Builder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orleans.Hosting;
+
+namespace Orleans.Runtime.Streams
+{
+    public class Sample
+    {
+        public void main()
+        {
+            var siloBuilder = new SiloHostBuilder()
+                .AddPersistentStream("EH")
+                .ConfigureQueueAdapter<AzureQueueAdapter, AzureQueueAdapterOptions>(options => ())
+                .ConfigureQueueBalancer<>
+                .Builder()
+        }
+    }
+}
